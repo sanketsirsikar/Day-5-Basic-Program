14419b1c7d28006c433349d13aea7d72faef5b87
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-13 23:29:00 +0530

    Swap two numbers

---
+++ src/com/Bridgelabz/BasicCore/SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+package com.Bridgelabz.BasicCore;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int temp;
+        int a = 10;
+        int b = 20;
+
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("after swap number  is  a= " + a);
+        System.out.println(" after swap number is b= " + b);
+    }
+}
+
