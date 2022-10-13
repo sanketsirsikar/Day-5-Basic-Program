613e77fff107206c04fc70cc3fbddaeb099bfab6
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-13 23:03:09 +0530

    check number is even or odd

--- src/com/Bridgelabz/BasicCore/CheckNumberEvenOrOdd.java
+++ src/com/Bridgelabz/BasicCore/CheckNumberEvenOrOdd.java
@@ -1,2 +1,21 @@
-package com.Bridgelabz.BasicCore;public class CheckNumberEvenOrOdd {
+package com.Bridgelabz.BasicCore;
+
+import java.util.Scanner;
+
+public class CheckNumberEvenOrOdd {
+    public static void EvenOrOdd(){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num % 2 ==0 )
+        {
+            System.out.println(num+ " number  is even");
+        }else
+        {
+            System.out.println(num + " number  is odd ");
+        }
+    }
+
+    public static void main(String[] args) {
+        EvenOrOdd();
+    }
 }
